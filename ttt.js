8fc2214fa58b4d14533f6c656774c60e777362d6
Young <willkdy@gmail.com>
2020-03-12 14:38:22 +0900

    Update ttt.js

--- ttt.js
+++ ttt.js
@@ -1,5 +1,5 @@
 $('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', '//cdn.jsdelivr.net/gh/2YoungKim/test/fluidbox.min.css') );
-$('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', '//cdn.jsdelivr.net/gh/2YoungKim/Fluidbox@7fad490154a3688a78bff8db72c786d78773bd01/src/css/_fluidbox.scss') );
+$('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', '//cdn.jsdelivr.net/gh/coreysnyder04/fluidbox-ghost-blog-plugin@8c04f1180f64a0e434236fdb7f8c28eb120ba395/fluidbox-ghost-blog-plugin.css'') );
 
 window.fluidboxGhost = $.when(
   $.getScript( "//cdnjs.cloudflare.com/ajax/libs/jquery-throttle-debounce/1.1/jquery.ba-throttle-debounce.min.js" ),
